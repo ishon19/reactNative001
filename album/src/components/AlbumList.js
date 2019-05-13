be5c8251fbbeb73d6cbaf76c994f9c4935b80242
shreyans <pathak.shreyans@gmail.com>
2019-05-13 14:14:02 +0530

    added api call

--- album/src/components/AlbumList.js
+++ album/src/components/AlbumList.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import axios from 'axios';
 
-const AlbumList = () => {
-	  const { textStyle, viewStyle } = styles; 
-  return (
-     <View> 
-       <Text>List of all the albums!!</Text>
-     </View>
-  	);
-};
+class AlbumList extends Component {
+
+   //initial state
+   state = { albums : [] };
+
+   componentWillMount(){
+   	console.log("Inside componentWillMount");
+   	axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+   	.then(response => this.setState({ albums : response.data }))
+   	.catch(error => console.log(error));
+
+   }
+
+   render(){
+	  return (
+	     <View> 
+	       <Text>List of all the albums!!</Text>
+	     </View>
+	  	);
+	}
+}
 
 const styles = {
 	textStyle : {
-      
        fontSize: 30,
        color: 'green'
 	},
